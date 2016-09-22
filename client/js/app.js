06968559dd7c840753dd164f32f64604d78da296
Walker <jwalkerlyle@gmail.com>
2016-09-22 17:51:06 -0500

    beautiful

--- client/js/app.js
+++ client/js/app.js
@@ -6,7 +6,9 @@ window.$ = window.jQuery = $;
 const app = document.querySelector('#app');
 
 // Set greeting
+
+const username = $('[data-bootstrap]').data('bootstrap').username;
 const greeting = document.createElement('h2');
-greeting.innerText = 'Hello ' + <%= username %> + '! Welcome to your Express Backbone App!';
+greeting.innerText = 'Hello ' + username + '! Welcome to your Express Backbone App!';
 
 app.appendChild(greeting);
